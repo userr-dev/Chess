9f7e9a76e7eb02dd2a9a8e18a2fb6907fb2803e0
Vitalii Voitovych <vitalii.voitovych@protonmail.com>
2026-06-20 17:37:08 +0300

    Rework Knight check evasion moves

--- Chess.Core/Pieces/Knight.cs
+++ Chess.Core/Pieces/Knight.cs
@@ -4,7 +4,7 @@ namespace Chess.Core.Pieces;
 
 public sealed class Knight : Piece
 {
-    private static readonly KnightMoveOffset[] MoveOffsets =
+    private static readonly HashSet<KnightMoveOffset> MoveOffsets =
         [(-1, 2), (1, 2), (-1, -2), (1, -2), (-2, 1), (-2, -1), (2, 1), (2, -1)];
     
     public Knight(Color color, Position position) : base(color, position)
@@ -19,6 +19,7 @@ public sealed class Knight : Piece
         var checkState = chessBoard.GetCheckState(Color);
         
         if (IsPinned || checkState.IsDoubleChecked) return new MoveResult(moves, attacks);
+        if (checkState.IsChecked) return GetCheckEvasionMoves(chessBoard, checkState, moves, attacks);
         
         foreach (var position in GetAttackedPositions(chessBoard))
         {
@@ -32,9 +33,30 @@ public sealed class Knight : Piece
             }
         }
 
-        return ApplyCheckFilter(checkState, moves, attacks);
+        return new MoveResult(moves, attacks);
     }
 
+    private MoveResult GetCheckEvasionMoves(ChessBoard chessBoard, CheckState checkState, List<Position> moves, List<Position> attacks)
+    {
+        Position[] targets = [..checkState.BlockingPositions, checkState.Attackers[0].Position];
+
+        foreach (var target in targets)
+        {
+            var offset = new KnightMoveOffset(
+                (int)target.Column - (int)Position.Column,
+                target.Row - Position.Row);
+
+            if (!MoveOffsets.Contains(offset)) continue;
+
+            if (!chessBoard[target].HasPiece)
+                moves.Add(target);
+            else if (chessBoard[target].HasEnemyPiece(Color))
+                attacks.Add(target);
+        }
+
+        return new MoveResult(moves, attacks);
+    }
+    
     internal override IEnumerable<Position> GetAttackedPositions(ChessBoard chessBoard)
     {
         foreach (var offset in MoveOffsets)
